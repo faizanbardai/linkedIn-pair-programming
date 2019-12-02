9e2f68fc44eafce0a90d0c457c70b42be9425a4a
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-02 15:44:05 +0100

    Added Modal

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer class="navbar fixed-bottom navbar-light bg-light">
-            <span class="navbar-text">
+        <footer className="navbar fixed-bottom navbar-light bg-light">
+            <span className="navbar-text">
                 LinkedIn &copy;2019 | Tash/FayJu 
             </span>
         </footer>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,20 +2,37 @@ import RetrieveMyProfile from '../API/RetrieveMyProfile'
 import React, { Component } from 'react'
 import { Card, Modal, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit  } from '@fortawesome/free-solid-svg-icons'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
 
 export default class Profile extends Component {
     state = {
         loading: true,
-        profile: ""
+        profile: "",
+        setShow: false
     }
+
+
+    handleClose() {
+        console.log("I'll close the modal");
+        this.setState({
+            setShow: false
+        })    
+    }
+    handleOpen = () => {
+        console.log("I'll open the modal");
+        this.setState({
+            setShow: true
+        })
+    }
+
     render() {
-        let { profile, loading } = this.state;
-        
+
+        let { loading, profile, setShow } = this.state;
+
         return (
             <>
                 {loading && <h1>Loading...</h1>}
-                
+
                 {profile && <section>
                     <Card body>
                         <div >
@@ -30,29 +47,31 @@ export default class Profile extends Component {
                         <div >
                             {profile.area}
                         </div>
-                        <div >
-                            <FontAwesomeIcon icon={ faEdit } />
+                        <div onClick={() => this.handleOpen()}>
+                            <FontAwesomeIcon icon={faEdit} />
                         </div>
                     </Card>
-                    {/* <Modal show={show} onHide={handleClose}>
+                    <Modal show={setShow} onHide={() => this.handleClose()}>
                         <Modal.Header closeButton>
-                            <Modal.Title>Modal heading</Modal.Title>
+                            <Modal.Title>Edit intro</Modal.Title>
                         </Modal.Header>
-                        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                        <Modal.Body>
+                            Show API data here.
+                        </Modal.Body>
                         <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
+                            <Button variant="secondary" onClick={() => this.handleClose()}>
                                 Close
                             </Button>
-                            <Button variant="primary" onClick={handleClose}>
+                            <Button variant="primary" onClick={() => this.handleClose()}>
                                 Save Changes
                             </Button>
                         </Modal.Footer>
-                    </Modal> */}
+                    </Modal>
                 </section>}
             </>
-            
+
         )
-        
+
     }
     componentDidMount = async () => {
         let response = await RetrieveMyProfile();
@@ -60,5 +79,8 @@ export default class Profile extends Component {
             profile: response,
             loading: false
         })
+        console.log("Component did mount. Loading: ", this.state.loading);
     }
+
+
 }
