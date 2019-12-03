d096a10b3664a6665d367cd95c6bfe453c53eb2e
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-03 14:04:35 +0100

    added Moment

--- src/Components/SingleExperience.jsx
+++ src/Components/SingleExperience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import Moment from 'react-moment';
 export default function SingleExperience(props) {
     let experience = props;
     console.log(experience);
@@ -9,7 +10,14 @@ export default function SingleExperience(props) {
                 <Card.Body>
                     <Card.Title>{role}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{company}</Card.Subtitle>
-                    <Card.Subtitle className="mb-2 text-muted">{startDate} - {endDate} (Add moment here)</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">
+                        <Moment format="MMM YYYY">{startDate}</Moment>&nbsp;-&nbsp;
+                        <Moment format="MMM YYYY">{endDate}</Moment><br />
+                        <Moment
+                            duration={startDate}
+                            date={endDate}
+                        />
+                    </Card.Subtitle>
                     <Card.Subtitle className="mb-2 text-muted">{area}</Card.Subtitle>
                     <Card.Text> {description}</Card.Text>
                 </Card.Body>
