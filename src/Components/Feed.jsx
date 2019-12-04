c1cdf392eb6fc84d413ce2bc362cc609437b7634
infinityshades <tash.aneiq@gmail.com>
2019-12-04 14:54:46 +0100

    fetching posts and adding cards

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,33 +1,57 @@
 import React, { Component } from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
+import GetAllPosts from '../API/GetAllPosts'
 export default class Feed extends Component {
-    render() {
-        return (
-            <>
-            <Form>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows="3" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
 
+  state = {
+    posts: []
+  }
+  render() {
+
+    let { allUsers, myProfile } = this.props
+    let { posts } = this.state
+
+    return (
+      <>
+        <Form>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+                </Button>
+        </Form>
+        {posts && posts.length > 0 &&
+          posts.map(post => (
             <Card >
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-          </>
-        )
-    }
+              <Card.Body>
+                <Image
+                  style={{ height: "30px" }}
+                  src={user.image}
+                  roundedCircle
+                />
+                <Card.Title>{post.username}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                  {post.text}
+                </Card.Text>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          ))
+        }
+      </>
+    )
+  }
+
+  componentDidMount = async () => {
+    this.setState({
+      posts: await GetAllPosts()
+    })
+    // let posts = await GetAllPosts ()
+    console.log(this.state.posts)
+  }
 }
 
