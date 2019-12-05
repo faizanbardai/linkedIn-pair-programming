4f1b50c492da546b32195cfd9136c17ee7a7e9a7
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-06 00:33:18 +0100

    showing img with each exp

--- src/Components/SingleExperience.jsx
+++ src/Components/SingleExperience.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Col, Image, Row } from 'react-bootstrap'
 import Moment from 'react-moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
@@ -11,28 +11,33 @@ export default class SingleExperience extends Component {
 
     render() {
         let { experience, personal } = this.props;
-        let { role, company, startDate, endDate, description, area, _id } = experience;
+        let { role, company, startDate, endDate, description, image, area, _id } = experience;
         return (
-            <Card.Body>
-                <div className="d-flex justify-content-between">
-                    <Card.Title>{role}</Card.Title>
-                    {personal && <div>
-                        <Button onClick={() => this.handleClick("delete", _id)} variant="outline-info" className="rounded-circle mx-2"><FontAwesomeIcon icon={faTrashAlt} /></Button>
-                        <Button onClick={() => this.handleClick("edit", _id)} variant="outline-info" className="rounded-circle mx-2"><FontAwesomeIcon icon={faPencilAlt} /></Button>
-                    </div>}
-                </div>
-                <Card.Subtitle className="mb-2 text-muted">{company}</Card.Subtitle>
-                <Card.Subtitle className="mb-2 text-muted">
-                    <Moment format="MMM YYYY">{startDate}</Moment>&nbsp;-&nbsp;
-                        <Moment format="MMM YYYY">{endDate}</Moment><br />
-                    <Moment
-                        duration={startDate}
-                        date={endDate}
-                    />
-                </Card.Subtitle>
-                <Card.Subtitle className="mb-2 text-muted">{area}</Card.Subtitle>
-                <Card.Text> {description}</Card.Text>
-            </Card.Body>
+            <Row>
+                <Col sm={12} md={4}><Image className="mb-2 mx-2" style={{ width: "100%" }} src={image} rounded /></Col>
+                <Col sm={12} md={8}>
+                    <Card.Body>
+                        <div className="d-flex justify-content-between">
+                            <Card.Title>{role}</Card.Title>
+                            {personal && <div>
+                                <Button onClick={() => this.handleClick("delete", _id)} variant="outline-info" className="rounded-circle mx-2 my-2"><FontAwesomeIcon icon={faTrashAlt} /></Button>
+                                <Button onClick={() => this.handleClick("edit", _id)} variant="outline-info" className="rounded-circle mx-2 my-2"><FontAwesomeIcon icon={faPencilAlt} /></Button>
+                            </div>}
+                        </div>
+                        <Card.Subtitle className="mb-2 text-muted">{company}</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">
+                            <Moment format="MMM YYYY">{startDate}</Moment>&nbsp;-&nbsp;
+                            <Moment format="MMM YYYY">{endDate}</Moment><br />
+                            <Moment
+                                duration={startDate}
+                                date={endDate}
+                            />
+                        </Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">{area}</Card.Subtitle>
+                        <Card.Text> {description}</Card.Text>
+                    </Card.Body>
+                </Col>
+            </Row>
         )
     }
 }
